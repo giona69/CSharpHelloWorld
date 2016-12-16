3196c460c532444d6e4ca3df1e02a57423013795
giona69 <giona.granata@gmail.com>
2016-12-16 17:45:22 +0100

    hello world

--- CSharpHelloWorld/Program.cs
+++ CSharpHelloWorld/Program.cs
@@ -10,6 +10,11 @@ namespace CSharpHelloWorld
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
